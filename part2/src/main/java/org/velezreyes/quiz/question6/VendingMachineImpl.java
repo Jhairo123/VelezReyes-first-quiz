b423c2fb89c6d8f00bbe31720fff336068a0274f
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2023-11-04 18:57:18 -0500

    Fix VendingMachineImpl.java

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,65 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import java.util.HashMap;
+import java.util.Map;
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
-  }
+public class VendingMachineImpl implements VendingMachine {
+    private Map<String, Integer> drinkPrices;
+    private int balance;
+
+    public VendingMachineImpl() {
+        drinkPrices = new HashMap<>();
+        drinkPrices.put("ScottCola", 75);
+        drinkPrices.put("KarenTea", 100);
+    }
+
+    public static VendingMachine getInstance() {
+        return new VendingMachineImpl();
+    }
+
+    @Override
+    public void insertQuarter() {
+        balance += 25;
+    }
+
+    @Override
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+        if (drinkPrices.containsKey(name)) {
+            int price = drinkPrices.get(name);
+            if (balance >= price) {
+                balance -= price;
+                if ("ScottCola".equals(name)) {
+                    return new Drink() {
+                        @Override
+                        public String getName() {
+                            return "ScottCola";
+                        }
+
+                        @Override
+                        public boolean isFizzy() {
+                            return true;
+                        }
+                    };
+                } else if ("KarenTea".equals(name)) {
+                    return new Drink() {
+                        @Override
+                        public String getName() {
+                            return "KarenTea";
+                        }
+
+                        @Override
+                        public boolean isFizzy() {
+                            return false;
+                        }
+                    };
+                }
+            } else {
+                throw new NotEnoughMoneyException();
+            }
+        } else {
+            throw new UnknownDrinkException();
+        }
+
+        return null;
+    }
 }
